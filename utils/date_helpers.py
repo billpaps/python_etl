ff6e60df0e3fc6e2003481fd0412201aefcb6941
vapapapostolou <vapapapostolou@gmail.com>
2024-05-19 22:29:36 +0300

    feat: add types to date_helpers.py

--- utils/date_helpers.py
+++ utils/date_helpers.py
@@ -1,20 +1,21 @@
 from datetime import datetime, timedelta
 
+# Helper functions for datetimes
 class DateHelpers:
 
     @staticmethod
-    def str_to_date(date):
+    def str_to_date(date: str):
         return datetime.strptime(date, "%Y-%m-%d")
     
     @staticmethod
-    def date_to_str(date):
+    def date_to_str(date: datetime):
         return datetime.strftime(date, "%Y-%m-%d")
 
     @staticmethod
-    def daterange(start_date, end_date):
+    def daterange(start_date: datetime, end_date: datetime):
         for n in range(int((end_date - start_date).days)):
             yield start_date + timedelta(n)
 
     @staticmethod
-    def timestamp_to_date(timestamp):
+    def timestamp_to_date(timestamp: int):
         return datetime.fromtimestamp(timestamp).date()
